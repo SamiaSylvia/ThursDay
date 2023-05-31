26bbce1bcaec2d1ec1f65f74efb0ca1d0be37220
samiasylvia <samiasylvia@localhost>
2023-05-31 18:51:02 -0400

    added java file for the feature

---
+++ src/test/java/steps/GoogleSearch2.java
@@ -0,0 +1,36 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoogleSearch2 {
+	
+	@Given("I am on the google homepage")
+	public void i_am_on_the_google_homepage() {
+
+		
+	}
+
+	@When("I enter search {string}")
+	public void i_enter_search(String string) {
+
+		
+		
+	}
+
+	@When("I click in google search button")
+	public void i_click_in_google_search_button() {
+
+		
+		
+	}
+
+	@Then("I receive related search result")
+	public void i_receive_related_search_result() {
+
+		
+		
+	}
+
+}
